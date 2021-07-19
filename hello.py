0951659c01e8f09521a8ab558f99fcbe82fad967
Wayne <72367965+waynexw@users.noreply.github.com>
2021-07-20 09:58:15 +1000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,28 @@
 print ("Hello world.")
 
+#
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+    
+@app.route("/api/NewPlayer")
+def NewPlayer():
+    return "Hello NewPlayer!"
+
+     
+@app.route("/api/checkUserName")
+def checkUserName():
+    return "Hello checkUserName!"
+
+     
+if __name__ == '__main__':
+    app.run(host="localhost", port=8080, debug=True)
+#
+
 """A Python Flask REST API BoilerPlate (CRUD) Style"""
 
 import argparse

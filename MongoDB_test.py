a0f3e0193d03cb2fe49688c636f15afe14d1f2ba
Wayne <72367965+waynexw@users.noreply.github.com>
2021-07-29 17:26:56 +1000

    Update MongoDB_test.py

--- MongoDB_test.py
+++ MongoDB_test.py
@@ -11,3 +11,16 @@ dblist = myclient.list_database_names()
 if "mydatabase" in dblist:
   
   print("The database exists.")
+
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+mydb = myclient["mydatabase"]
+
+mycol = mydb["customers"]
+
+collist = mydb.list_collection_names()
+
+if "customers" in collist:
+  
+  print("The collection exists.")

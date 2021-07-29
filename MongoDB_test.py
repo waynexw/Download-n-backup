52cde733b2dce2eddb13c24503ad57662ab1fe6b
Wayne <72367965+waynexw@users.noreply.github.com>
2021-07-29 17:25:40 +1000

    Create MongoDB_test.py

---
+++ MongoDB_test.py
@@ -0,0 +1,13 @@
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+mydb = myclient["mydatabase"]
+
+print(myclient.list_database_names())
+
+dblist = myclient.list_database_names()
+
+if "mydatabase" in dblist:
+  
+  print("The database exists.")

f599dec11a352c69e6f5db2c2f68a13509d321af
Wayne <72367965+waynexw@users.noreply.github.com>
2021-08-04 10:32:21 +1000

    Update MongoDB_test.py

--- MongoDB_test.py
+++ MongoDB_test.py
@@ -71,3 +71,17 @@ mycol = mydb["customers"]
 
 for x in mycol.find():
   print(x)
+
+# Find document(s) with the address "Park Lane 38":
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+myquery = { "address": "Park Lane 38" }
+
+mydoc = mycol.find(myquery)
+
+for x in mydoc:
+  print(x)

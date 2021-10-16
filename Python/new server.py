25f58eea53764196408d818f3e912663a3a58d0b
Wayne <72367965+waynexw@users.noreply.github.com>
2021-10-16 11:07:12 +1100

    Update new server.py

--- Python/new server.py
+++ Python/new server.py
@@ -1,9 +1,11 @@
 import urllib.request
 url = 'http://news.baidu.com/sports'
 
-#创建request对象
+
+# 创建request对象 
 req = urllib.request.Request(url)
-#发送请求，获取结果
+
+# 发送请求，获取结果
 with urllib.request.urlopen(req) as response:
 data = response.read()
 content = data.decode()
